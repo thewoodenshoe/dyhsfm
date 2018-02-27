18abe37bbf634352727ee9ebcaf38a8ee9cf0204
stewartpa <stewart.paul@gmail.com>
2018-02-26 20:20:26 -0500

    added lost password

--- dyhsfm/src/Home.js
+++ dyhsfm/src/Home.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-
+import { Panel } from "react-bootstrap";
+import "./Home.css";
 class Home extends Component {
     render() {
         return (
-             <div>
-                 Hello, this is the main page.
+            <div>
+               
+                    <Panel className="Home-panel">
+                        <Panel.Heading>
+                            Heading
+                        </Panel.Heading>
+                        <Panel.Body>
+                            Hello, this is the main page
+                        </Panel.Body>
+                        <Panel.Footer>
+                            Footer
+                        </Panel.Footer>
+                    </Panel>
             </div>
         )
     }
--- dyhsfm/src/Login.js
+++ dyhsfm/src/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Button, FormGroup, FormControl, ControlLabel, Panel } from "react-bootstrap";
 import "./Login.css";
-var Alert = require('react-bootstrap/lib/Alert');
 class Login extends Component {
     constructor(props){
     super(props);
@@ -9,12 +8,13 @@ class Login extends Component {
       email: "",
       password: "",
       RegisterOpen: false,
+      LostPasswordOpen: false
     };
   }
   validateLogin() {
     return this.state.email.length > 0 && this.state.password.length > 0;
   }
-  validateRegister() {
+  validateEmail() {
     return this.state.email.length > 0;
   }
  handleChange = event => {
@@ -60,11 +60,12 @@ class Login extends Component {
                   </Button>      
                 </form>
                 </Panel.Body>
+
                 <Panel.Footer>
-                <Button onClick={() => this.setState({ RegisterOpen: !this.state.RegisterOpen })}>
-                  Don't have an account yet? Register here
+
+                <Button className="Login" onClick={() => this.setState({ RegisterOpen: !this.state.RegisterOpen })}>
+                  Register account
                 </Button>
-                <br />
                 <Panel id="collapsible-register-panel" expanded={this.state.RegisterOpen}>
                 <Panel.Collapse>
                 <Panel.Body>
@@ -80,8 +81,34 @@ class Login extends Component {
                         onChange={this.handleChange}
                       />
                     </FormGroup>
-                    <Button bsStyle="success" block bsSize="large" disabled={!this.validateRegister()} type="submit"> 
+                    <Button bsStyle="success" block bsSize="large" disabled={!this.validateEmail()} type="submit"> 
                       Register
+                    </Button>      
+                  </form>
+                </Panel.Body>
+                </Panel.Collapse>
+                </Panel> 
+
+                <Button className="Login" onClick={() => this.setState({ LostPasswordOpen: !this.state.LostPasswordOpen })}>
+                  Lost password
+                </Button>
+                <Panel id="collapsible-lostPassword-panel" expanded={this.state.LostPasswordOpen}>
+                <Panel.Collapse>
+                <Panel.Body>
+                  <form onSubmit={this.LostPassword}>
+                    <FormGroup controlId="email" bsSize="large">
+                      <ControlLabel>
+                        Email: 
+                      </ControlLabel>
+                      <FormControl
+                        autoFocus
+                        type="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                      />
+                    </FormGroup>
+                    <Button bsStyle="success" block bsSize="large" disabled={!this.validateEmail()} type="submit"> 
+                      Reset password
                     </Button>      
                   </form>
                 </Panel.Body>
