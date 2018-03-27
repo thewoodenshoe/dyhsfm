fc7605de036c9661b92a5dca36f9776f2a5fb501
stewartpa <stewart.paul@gmail.com>
2018-03-26 21:35:48 -0400

    latest greatest

--- dyhsfm/routes/DatabaseApi.js
+++ dyhsfm/routes/DatabaseApi.js
@@ -1,43 +1,53 @@
 var mysql      = require('mysql');
 
-//
 // dummy dbase and password for now. can be seen in GIT, i know.
-//
-
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'stewart01', 
   database : 'dyhsfm',
-});
-connection.connect(function(err){
-if(!err) {
-    console.log("Database is connected.");
-} else {
-    console.log("Error connecting database: "+err);
-}
-});
+})
 
-exports.login = function(req,res){
-    var email= req.body.email;
-    var password = req.body.password;
-    connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
+connection.connect(function(err){
+  if(!err) {
+     console.log("Database is connected.")
+  }
+  else {
+    connection.password = ''
+    connection.connect(function(err){
+      if(!err) {
+        console.log("Database is connected (no pw).")
+      }
+      else {
+        console.log("Error connecting database: " +err)
+      }
+    })
+  }
+})
+
+ // exports.login = function(email, password){
+ //   connection.query('select * from users where email = ?',[email], function (error, results, fields) {
+ exports.login = function(req,res){
+    let email= req.body.email
+    let password = req.body.password
+    connection.query('select * from users where email = ?',[email], function (error, results, fields) {
     if (error) {
-      // console.log("error ocurred",error);
+      console.log("error ocurred",error);
       res.send({
         "code":400,
         "failed":"error ocurred"
       })
-    }else{
-      // console.log('The solution is: ', results);
+    } else {
       if(results.length >0){
         if([0].password == password){
+          console.log('login sucessfull')
           res.send({
             "code":200,
             "success":"login sucessfull"
               });
         }
         else{
+          console.log('Email and password does not match')
           res.send({
             "code":204,
             "success":"Email and password does not match"
@@ -45,9 +55,10 @@ exports.login = function(req,res){
         }
       }
       else{
+        console.log('email does not exists')
         res.send({
           "code":204,
-          "success":"Email does not exits"
+          "success":"Email does not exists"
             });
       }
     }
@@ -58,14 +69,14 @@ exports.login = function(req,res){
     // console.log("req",req.body);
     var today = new Date();
     var users={
-      "first_name":req.body.first_name,
-      "last_name":req.body.last_name,
-      "email":req.body.email,
-      "password":req.body.password,
-      "created":today,
-      "modified":today
+      "first_name" : req.body.firstName,
+      "last_name"  : req.body.lastName,
+      "email"      : req.body.email,
+      "password"   : 'stewart01',
+      "created"    : today,
+      "modified"   : today
     }
-    connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
+    connection.query('insert into users set ?',users, function (error, results, fields) {
     if (error) {
       console.log("error ocurred",error);
       res.send({
@@ -73,7 +84,7 @@ exports.login = function(req,res){
         "failed":"error ocurred"
       })
     }else{
-      console.log('The solution is: ', results);
+      console.log('Insert successfull: ', results);
       res.send({
         "code":200,
         "success":"user registered sucessfully"
--- dyhsfm/server.js
+++ dyhsfm/server.js
@@ -10,34 +10,12 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.post('/mytest', function (req, res) {
-  // the objective is to redirect to DatabaseApi.Login and return a session ID.
-  // so my though process is: capture the parameters (email and password)
-  // then send them to the login function.
+app.post('/login', dbApi.login)
 
-  // step 1: capture parameters.
-  // trying option 1, no result 
-  console.log('In server 1: ' + JSON.stringify(req.body.email)) // returns undefined
-  console.log('In server 2: ' + JSON.stringify(req.body.email))  // returns undefined
-  
-  
-  // below will return: n server 3: {"{\"email\":\"stewart.paul@gmail.com\"}":""}  (good news)
-  // so i think i gotta work with this. I get TEXT (req.body is always text and i stringified it in the client)
-  console.log('In server 3: ' + JSON.stringify(req.body))
-  // so i want to do something like this. 
-  let myEmail = JSON.stringify(req.body.email)
-  // above doesnt work, so i figured I need to parse the text  to JSON first. But below crashes on a parse error?
-  // I figured I dont sent it correctly in my client (login.js?) but it seems right?!
-  let myObj = JSON.parse(req.body)
-
-
-  
-  // dummy return
-  return res.send('{id: 1}');
-});
+app.post('/register', dbApi.register)
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+})
 
 app.listen(8080, () => console.log('Example app listening on port 8080'))
